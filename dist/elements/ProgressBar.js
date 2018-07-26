e6bfa776602151275a8b241c718a16858dbb718d
Jay Honnold <jayhonnold@gmail.com>
2018-07-26 15:32:29 -0500

    Addition of react-motion for smoother transitions

--- dist/elements/ProgressBar.js
+++ dist/elements/ProgressBar.js
@@ -4,9 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _reactMotion = require('react-motion');
 
 var _propTypes = require('prop-types');
 
@@ -18,7 +22,11 @@ var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var ProgressWrapper = _styledComponents2.default.div.withConfig({
   displayName: 'ProgressWrapper'
@@ -29,24 +37,51 @@ var ProgressWrapper = _styledComponents2.default.div.withConfig({
 
 var Progress = _styledComponents2.default.div.withConfig({
   displayName: 'Progress'
-})(['background-color:', ';height:100%;width:', '%;border-radius:0.5rem;transition:width 0.25s linear;'], function (_ref2) {
+})(['background-color:', ';height:100%;border-radius:0.5rem;'], function (_ref2) {
   var theme = _ref2.theme;
   return theme.primary;
-}, function (_ref3) {
-  var percent = _ref3.percent;
-  return percent || 0;
 });
 
-var ProgressBar = function ProgressBar(_ref4) {
-  var percent = _ref4.percent,
-      props = _objectWithoutProperties(_ref4, ['percent']);
+var ProgressBar = function (_React$Component) {
+  _inherits(ProgressBar, _React$Component);
 
-  return _react2.default.createElement(
-    ProgressWrapper,
-    props,
-    _react2.default.createElement(Progress, { percent: percent })
-  );
-};
+  function ProgressBar() {
+    _classCallCheck(this, ProgressBar);
+
+    return _possibleConstructorReturn(this, (ProgressBar.__proto__ || Object.getPrototypeOf(ProgressBar)).apply(this, arguments));
+  }
+
+  _createClass(ProgressBar, [{
+    key: 'render',
+    value: function render() {
+      var percent = this.props.percent;
+
+
+      return _react2.default.createElement(
+        ProgressWrapper,
+        this.props,
+        _react2.default.createElement(
+          _reactMotion.Motion,
+          { style: { percent: (0, _reactMotion.spring)(percent) } },
+          function (_ref3) {
+            var percent = _ref3.percent;
+            return _react2.default.createElement(Progress, { style: { width: percent + '%' } });
+          }
+        )
+      );
+    }
+  }]);
+
+  return ProgressBar;
+}(_react2.default.Component);
+
+// const ProgressBar = ({ percent, ...props }) => (
+//   <ProgressWrapper {...props}>
+//     <Motion defaultStyle={{ >
+//       <Progress percent={percent} />
+//     </Motion>
+//   </ProgressWrapper>
+// );
 
 ProgressBar.propTypes = {
   percent: _propTypes2.default.number
--- lib/elements/ProgressBar.jsx
+++ lib/elements/ProgressBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Motion, spring } from 'react-motion';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -15,18 +16,31 @@ const Progress = styled.div`
   background-color: ${({ theme }) => theme.primary};
 
   height: 100%;
-  width: ${({ percent }) => percent || 0}%;
 
   border-radius: 0.5rem;
-
-  transition: width 0.25s linear;
 `;
 
-const ProgressBar = ({ percent, ...props }) => (
-  <ProgressWrapper {...props}>
-    <Progress percent={percent} />
-  </ProgressWrapper>
-);
+class ProgressBar extends React.Component {
+  render() {
+    const { percent } = this.props;
+
+    return (
+      <ProgressWrapper {...this.props}>
+        <Motion style={{ percent: spring(percent) }}>
+          {({ percent }) => <Progress style={{ width: `${percent}%` }} />}
+        </Motion>
+      </ProgressWrapper>
+    );
+  }
+}
+
+// const ProgressBar = ({ percent, ...props }) => (
+//   <ProgressWrapper {...props}>
+//     <Motion defaultStyle={{ >
+//       <Progress percent={percent} />
+//     </Motion>
+//   </ProgressWrapper>
+// );
 
 ProgressBar.propTypes = {
   percent: PropTypes.number,
